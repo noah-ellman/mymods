b23cbe3c2d9223badd34bb9ed9048c22c508e320
Noah Ellman <noahellman@gmail.com>
2017-03-24 00:46:22 -0700

    initial

--- bingle-search/lib/google-search-api.js
+++ bingle-search/lib/google-search-api.js
@@ -1,3 +1,37 @@
-/**
- * Created by Noah on 3/19/2017.
- */
+const GoogleSearch = require('wko-google-search');
+const googleSearch = new GoogleSearch({
+    key: 'AIzaSyBLYPC8T5w1S-mnxG2xU5SrJQb0JOF4g9k',
+    cx: '002201091616557569186:ie9zmupj0pa'
+});
+
+
+
+
+function search(query, options, callback) {
+
+
+    googleSearch.fetch(query, (err, hits) => {
+        console.log(hits);
+
+        const data = [];
+
+        hits.forEach( v => {
+            data.push(
+                {
+                    title: v.title,
+                    href: v.link,
+                    desc: v.snippet
+                }
+            )
+        });
+
+        return callback(data);
+    });
+
+
+
+
+}
+
+
+module.exports.search = search;
